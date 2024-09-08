17759e25c88ff7be54e33e114ab78b64a0cf3255
Kanishk <kanishka.ranjan.9@gmail.com>
2024-09-09 00:45:45 +0530

    authentication done

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -82,7 +82,7 @@ const MobileNav = ({ user }: MobileNavProps) => {
               </nav>
             </SheetClose>
             
-            <Footer user= {user}/>
+            <Footer user= {user} type ="mobile"/>
             
           </div>
         </SheetContent>
